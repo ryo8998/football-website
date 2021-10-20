b8a3427657e399f93c51d42c2ff75ddc00dbff41
ryo8998 <ishino.r8998@gmail.com>
2021-10-20 16:17:11 +0900

    reset pass

--- sports_fan_website/settings.py
+++ sports_fan_website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from .local_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2r@m5_+(d%s6!ocsz94361n&%mz$7vok0n@jx_wlisy-%6o_+6'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
